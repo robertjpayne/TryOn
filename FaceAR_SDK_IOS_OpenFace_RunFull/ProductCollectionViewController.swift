cfa8e55a4cd9adfafb2bf371774af1e12af25ebb
chrisdunaetz <chris@dunaetz.com>
2017-04-10 13:21:52 -0700

    Workspace setup

--- FaceAR_SDK_IOS_OpenFace_RunFull/ProductCollectionViewController.swift
+++ FaceAR_SDK_IOS_OpenFace_RunFull/ProductCollectionViewController.swift
@@ -18,10 +18,11 @@ class ProductCollectionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       pageControl.numberOfPages = products.count
+        pageControl.numberOfPages = products.count
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
         return products.count
     }
     
@@ -35,10 +36,12 @@ class ProductCollectionViewController: UIViewController {
     }
     
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ProductCollectionViewCell
     }
     
     func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ProductCollectionViewCell
     }
     
@@ -48,19 +51,18 @@ class ProductCollectionViewController: UIViewController {
         
         return view.frame.size
     }
-    var pageX = 0 {
-        didSet {
-            collectionView.reloadData()
-        }
-    }
+
     func scrollViewDidScroll(_ scrollView: UICollectionView) {
+        
+        //Here we can tell when the cell has landed exactly in the middle of the screen.
         let middlePoint = CGPoint(x: scrollView.frame.width/2 + scrollView.contentOffset.x, y: scrollView.frame.height/2 + scrollView.contentOffset.y)
         if let centerRowIndex = scrollView.indexPathForItem(at: middlePoint) {
             for index in scrollView.indexPathsForVisibleItems {
                 if index.row == centerRowIndex.row {
                     UIView.animate(withDuration: 0.4, animations: {
+                        
+                        //We update the page control to show which cell we're on.
                         self.pageControl.currentPage = index.row
-//                        self.pageX = index.row
                     })
                 }
             }
---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TryOn' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TryOn
+
+    pod ‘Alamofire’
+    pod 'MBProgressHUD'
+    pod ‘SnapKit’
+    pod ‘SwiftyJSON’
+
+  target 'TryOnTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TryOnUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
